85956874a3cef0a98ba41b25fdbf06e6c15e2e03
Christan Fergus <Fergus@Christans-MacBook-Pro.local>
2016-06-12 18:29:24 -0600

    initial coding of site and design

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -2,4 +2,13 @@ $('.toggle').click(function(){
 	$(this).toggleClass('active');
 });
 
+// var waypoint = new Waypoint({
+//   element: document.getElementById('waypoint'),
+//   handler: function(direction) {
+//     console.log('Scrolled to waypoint!')
+//   }
+// });
 
+var sticky = new Waypoint.Sticky({
+	element: $('.header-wrapper')[0]
+});
